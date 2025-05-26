fc849648ab1574cb38e54b41526a58be9ec0c2d9
Nimitt <nimittnim@gmail.com>
2025-05-27 04:12:28 +0530

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+window.MathJax = {
+  tex: {
+    macros: {
+      Pr: "\\mathbb{P}",
+      E: "\\mathbb{E}",
+      Var: "\\mathrm{Var}",
+      Cov: "\\mathrm{Cov}",
+      Ber: "\\mathrm{Bernoulli}",
+      Bin: "\\mathrm{Binomial}",
+      Geo: "\\mathrm{Geometric}",
+      Poisson: "\\mathrm{Poisson}",
+      Unif: "\\mathrm{Uniform}",
+      Norm: "\\mathcal{N}",
+      Exp: "\\mathrm{Exp}",
+      indep: "\\perp\\!\\!\\!\\perp",
+      given: "\\mid",
+      R: "\\mathbb{R}",
+      N: "\\mathbb{N}",
+      Z: "\\mathbb{Z}",
+      G:"\\mathbb{G}",
+      Q: "\\mathbb{Q}",
+      S: "\\mathbb{S}",
+      "1": "\\mathbf{1}",
+      argmax: "\\operatorname*{arg\\,max}",
+      argmin: "\\operatorname*{arg\\,min}"
+    },
+    displayAlign: "left"
+  }
+};
+
+
